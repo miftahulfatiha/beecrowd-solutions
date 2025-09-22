b3b16537c2b587a490b15c52fa57c7fbc8a61f3e
miftahulfatiha <fatihamifta@gmail.com>
2025-09-22 16:29:47 +0600

    Add files via upload

---
+++ 1010-simplecalculate.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long a,b,d,e;
+    double c,h;
+    cin>>a>>b>>c;
+    cin>>d>>e>>h;
+    cout<<fixed<<setprecision(2)<<"VALOR A PAGAR: R$ " <<(b*c)+(e*h)<<endl;
+}
